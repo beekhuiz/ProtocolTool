17083a44b25a130cbb552ae75008a154c75182c0
Maarten Pronk <git@evetion.nl>
2016-06-14 14:14:28 +0200

    Added automatic wsgi restart code.

--- main/wsgi.py
+++ main/wsgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+import main.monitor
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+main.monitor.start(interval=20.0)
