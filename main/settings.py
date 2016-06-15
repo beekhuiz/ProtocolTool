22b120f61b0478317038361a334c373152802e30
beekhuiz <johan.beekhuizen@deltares.nl>
2016-06-15 14:33:24 +0200

    Improved table css and added dir static to settings

--- main/settings.py
+++ main/settings.py
@@ -94,13 +94,9 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Amsterdam'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -108,3 +104,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
